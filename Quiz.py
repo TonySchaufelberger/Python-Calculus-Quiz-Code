1e2b258550bbb4e72e1457ab017dbe0c19d563f6
TonySchaufelberger <67718063+TonySchaufelberger@users.noreply.github.com>
2020-07-03 14:31:58 +1200

    Add files via upload

---
+++ Quiz.py
@@ -0,0 +1,33 @@
+import tkinter as tk
+
+large_font = ("Verdana", 12)
+
+class rootFrame (tk.Tk):
+        """"""
+        def __init__(self, *args, **kwargs):
+                tk.Tk.__init__(self, *args, **kwargs)
+                container = tk.Frame(self)
+                container.pack(side="top", fill="both", expand=True)
+                container.grid_rowconfigure(0, weight=1)
+                container.grid_columnconfigure(0, weight=1)
+                
+                self.frames = {}
+                frame = startingPage(container, self)
+                self.frames[startingPage] = frame
+                frame.grid(row=0, column=0, sticky="nsew")
+
+                self.show_frame(startingPage)
+
+        def show_frame (self, cont):
+                frame = self.frames[cont]
+                frame.tkraise()
+
+class startingPage (tk.Frame):
+        """"""
+        def __init__ (self, parent, controller):
+                tk.Frame.__init__(self, parent)
+                label = tk.Label(self, text="This is the start page", font=LARGE_FONT)
+                label.pack(pady=10,padx=10)
+
+quiz = rootFrame()
+quiz.mainloop()
