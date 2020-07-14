551ee32450e0867fd528adeb1e007aec760cc56d
TonySchaufelberger <67718063+TonySchaufelberger@users.noreply.github.com>
2020-07-14 13:41:53 +1200

    Update Quiz.py

--- Quiz.py
+++ Quiz.py
@@ -1,45 +1,48 @@
 import tkinter as tk
 
-class section():
-        """"""
-        
-        def __init__ (self, questions, question_type):
-                self.questions = questions
-                self.question_type = question_type
+LARGE_FONT = ("Verdana", 12)
 
-class rootFrame(tk.Frame):
+class rootFrame(tk.Tk):
         """"""
-        
-        def __init__ (self, parent, *args, **kwargs):
-                tk.Frame.__init__(self, parent, *args, **kwargs)
-                self.parent = parent
-                self.introFrame = introFrame(self)
-                self.questionFrame = questionFrame(self)
-                
-        def grid_frame (frame):
-                pass
-                
-        def storeInformation ():
-                pass
-        
-class introFrame(tk.Frame):
-        """"""
-        
-        def __init__ (self, parent, *args, **kwargs):
-                tk.Frame.__init__(self, parent, *args, **kwargs)
-                pass
-                
-class questionFrame(tk.Frame):
-        """"""
-        
-        def __init__ (self, *question_list):
-                self.question_list = question_list
-                pass
-                
-        def grid_multiple ():
-                for i in questions:
-                        self.grid_frame(i);
+        def __init__(self, *args, **kwargs):
+                tk.Tk.__init__(self, *args, **kwargs)
+                container = tk.Frame(self)
+                container.pack(side="top", fill="both", expand=True)
+                container.grid_rowconfigure(0, weight=1)
+                container.grid_columnconfigure(0, weight=1)
+                container.grid_columnconfigure(1, weight=1)
 
-root = tk.Tk()
-rootFrame(root).pack(side="top", fill="both", expand=True)
-root.mainloop()
+                self.frames = {}
+                frame = startingPage(container, self)
+                self.frames[startingPage] = frame
+                frame.grid(row=0, column=0, sticky="nsew")
+
+                self.show_frame(startingPage)
+
+        def show_frame(self, cont):
+                frame = self.frames[cont]
+                frame.tkraise()
+
+def test(message):
+        print(message)
+
+class startingPage(tk.Frame):
+        """"""
+        def __init__(self, parent, controller):
+                tk.Frame.__init__(self, parent)
+                label = tk.Label(self, text="This is the start page", font=LARGE_FONT)
+                label.grid(row=0,column=0)
+
+                button = tk.Button(self, text="Next", command=lambda: test("hi"))
+                button.grid(row=0,column=1)
+
+class selectionPage(tk.Frame):
+        """"""
+        def __init__(self, parent, controller):
+                tk.Frame.__init__(self, parent)
+                label = tk.Label(self, text="This is the section page", font=LARGE_FONT)
+                label.pack(pady=10,padx=10)
+
+quiz = rootFrame()
+quiz.geometry("500x300")
+quiz.mainloop()
