e7e459e20d5d34a3cfde64fe6b30c4f9af43a00c
TonySchaufelberger <67718063+TonySchaufelberger@users.noreply.github.com>
2020-07-18 11:26:16 +1200

    Adding question frames and quiz functionality
    Variables for whether complex number, differentiation and integration questions will be added to the quiz. A new questionPage class, which iterates through the questionPage instances. The rootFrame now has a generate_quiz method, which initializes the questions into the group of frames, each question it's own instance with it's own number and later question.

--- Quiz.py
+++ Quiz.py
@@ -4,13 +4,14 @@ from tkinter import ttk
 LARGE_FONT = ("Verdana", 12)
 
 def combine_funcs(*funcs):
+        """A function designed to return multiple functions for the tkinter button commands"""
         def combined_func(*args, **kwargs):
                 for f in funcs:
                         f(*args, **kwargs)
         return combined_func
 
 class ok_popup:
-        """"""
+        """Simply opens a new tkinter window when called. Its only button/method is the OK button."""
         def __init__(self, msg):
                 self.msg = msg
                 self.popup_box = tk.Tk()
@@ -25,7 +26,10 @@ class ok_popup:
                 button.pack()
 
 class quit_popup(ok_popup):
-        """"""
+        """A child of the ok_popup class, inherits everything but changes the buttons into Yes and No,
+        Yes: closes all windows
+        No: closes only the popup
+        The yes_function method is used for the yes button, is overrided in the child class"""
         def __init__(self, msg, parent):
                 self.parent = parent
                 super().__init__(msg)
@@ -36,8 +40,13 @@ class quit_popup(ok_popup):
                 button.pack()
                 button2.pack()
 
+        def yes_function(self):
+                pass
+
 class cancel_popup(quit_popup):
-        """"""
+        """A child of the quit_popup, inherits the same buttons but adds new functionality for the first button
+        Yes: yes_function
+        No: closes the popup"""
         def __init__(self, msg, parent, func):
                 self.func = func
                 super().__init(msg, parent)
@@ -47,21 +56,30 @@ class cancel_popup(quit_popup):
 
 
 class rootFrame(tk.Tk):
-        """"""
+        """The frame that stores all important information, and is parent to all other frames
+        The show_frame method raises the selected frame to the top
+        The generate_quiz method generates a series of frames each containing a unique question, based on the types of questions selected"""
+
         def __init__(self, *args, **kwargs):
                 tk.Tk.__init__(self, *args, **kwargs)
                 tk.Tk.iconbitmap(self, default="")
                 tk.Tk.wm_title(self, "Level 3 Calculus Revision Quiz")
-                container = tk.Frame(self)
-                container.pack(side="top", fill="both", expand=True)
-                container.grid_rowconfigure(0, weight=1)
-                container.grid_columnconfigure(0, weight=1)
-                container.grid_columnconfigure(1, weight=1)
+
+                # I've converted container to self.container so I can access it in a different method, hopefully I will find a way to revert it
+                self.container = tk.Frame(self)
+                self.container.pack(side="top", fill="both", expand=True)
+                self.container.grid_rowconfigure(0, weight=1)
+                self.container.grid_columnconfigure(0, weight=1)
+                self.container.grid_columnconfigure(1, weight=1)
+
+                self.complex_test = tk.BooleanVar(self)
+                self.differentiation_test = tk.BooleanVar(self)
+                self.integration_test = tk.BooleanVar(self)
 
                 self.frames = {}
 
                 for f in (startingPage, selectionPage):
-                        frame = f(container, self)
+                        frame = f(self.container, self)
                         self.frames[f] = frame
                         frame.grid(row=0, column=0, sticky="nsew")
 
@@ -71,8 +89,15 @@ class rootFrame(tk.Tk):
                 frame = self.frames[cont]
                 frame.tkraise()
 
+        def generate_quiz(self, questions):
+                """This method is the same as the for loop in the __init__, except it passes each question as its own instance"""
+                for question in range(10):
+                        frame = questionPage(self.container, self, question)
+                        self.frames["questionPage" + str(question)] = frame
+                        frame.grid(row=0, column=0, sticky="nsew")
+
 class startingPage(tk.Frame):
-        """"""
+        """This page contains a next button to the selection page"""
         def __init__(self, parent, controller):
                 tk.Frame.__init__(self, parent)
                 label = ttk.Label(self, text="This is the start page", font=LARGE_FONT)
@@ -84,13 +109,34 @@ class startingPage(tk.Frame):
                 popup_button = ttk.Button(self, text="popup", command=lambda: quit_popup("close", controller))
                 popup_button.grid(row=0,column=2)
 
-
 class selectionPage(tk.Frame):
-        """"""
+        """This page contains check buttons to toggle which type of question will be asked in the quiz"""
         def __init__(self, parent, controller):
                 tk.Frame.__init__(self, parent)
                 label = ttk.Label(self, text="This is the section page", font=LARGE_FONT)
                 label.pack(pady=10,padx=10)
+
+                complex_check = ttk.Checkbutton(self, text="Test Complex Numbers?", variable=controller.complex_test)
+                differentiation_check = ttk.Checkbutton(self, text="Test Differentiation?", variable=controller.differentiation_test)
+                integration_check = ttk.Checkbutton(self, text="Test Integration?", variable=controller.integration_test)
+                complex_check.pack()
+                differentiation_check.pack()
+                integration_check.pack()
+
+                button = ttk.Button(self, text="Start Quiz", command=lambda: combine_funcs(controller.generate_quiz(0), controller.show_frame("questionPage0")))
+                button.pack()
+
+class questionPage(tk.Frame):
+        """This is the general frame for questions, which will change depending on the number and type of question asked
+        There will always be multiple instances of this object"""
+        def __init__(self, parent, controller, number):
+                tk.Frame.__init__(self, parent)
+                text = "A question" + str(number+1)
+                label = ttk.Label(self, text=text, font=LARGE_FONT)
+                label.pack(pady=10,padx=10)
+
+                button = ttk.Button(self, text="next question", command=lambda: controller.show_frame("questionPage" + str(number+1)))
+                button.pack()
 
 quiz = rootFrame()
 quiz.geometry("500x300")
