ef762292de0d32fd610f1facf560c26c5c321ef4
TonySchaufelberger <67718063+TonySchaufelberger@users.noreply.github.com>
2020-07-29 10:38:27 +1200

    A file for storing the questions of the quiz
    This reduces sustainability problems in the original, and makes it more neat and tidy.

---
+++ variables.py
@@ -0,0 +1,645 @@
+differentiation_questions = {"easy": [
+  {
+    "question": "differentiation:What is the conjugate of 1 + 2i?",
+    "answers": {
+      "a": '2i',
+      "b": '(-1 - 2i)',
+      "c": '(-1 + 2i)',
+      "d": '(1 - 2i)'
+    },
+    "correct_answer": 'd'
+  },
+  {
+    "question": "differentiation:Simplify (i^3).",
+    "answers": {
+      "a": '(-i)',
+      "b": '(i)',
+      "c": '(-1)',
+      "d": '(1)'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:easy3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:easy4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:easy5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:easy6",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:easy7",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:easy8",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }],
+        "medium": [
+  {
+    "question": "differentiation:test",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium1",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium2",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium6",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:medium7",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }],
+        "hard": [
+  {
+    "question": "differentiation:hard1",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:hard2",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:hard3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:hard4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "differentiation:hard5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }]
+}
+
+integration_questions = {"easy": [
+  {
+    "question": "integration:What is the conjugate of 1 + 2i?",
+    "answers": {
+      "a": '2i',
+      "b": '(-1 - 2i)',
+      "c": '(-1 + 2i)',
+      "d": '(1 - 2i)'
+    },
+    "correct_answer": 'd'
+  },
+  {
+    "question": "integration:Simplify (i^3).",
+    "answers": {
+      "a": '(-i)',
+      "b": '(i)',
+      "c": '(-1)',
+      "d": '(1)'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:easy3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:easy4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:easy5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:easy6",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:easy7",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:easy8",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }],
+        "medium": [
+  {
+    "question": "integration:test",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium1",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium2",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium6",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:medium7",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }],
+        "hard": [
+  {
+    "question": "integration:hard1",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:hard2",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:hard3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:hard4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "integration:hard5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }]
+}
+
+complex_questions = {
+        "easy": [
+  {
+    "question": "complex:What is the conjugate of 1 + 2i?",
+    "answers": {
+      "a": '2i',
+      "b": '(-1 - 2i)',
+      "c": '(-1 + 2i)',
+      "d": '(1 - 2i)'
+    },
+    "correct_answer": 'd'
+  },
+  {
+    "question": "complex:Simplify (i^3).",
+    "answers": {
+      "a": '(-i)',
+      "b": '(i)',
+      "c": '(-1)',
+      "d": '(1)'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:easy3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:easy4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:easy5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:easy6",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:easy7",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:easy8",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }],
+        "medium": [
+  {
+    "question": "complex:test",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium1",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium2",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium6",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:medium7",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }],
+        "hard": [
+  {
+    "question": "complex:hard1",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:hard2",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:hard3",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:hard4",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "complex:hard5",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd'
+    },
+    "correct_answer": 'a'
+  }]
+}
