8a6f278e3a8380e590ff7afd5d21d04225cf9194
TonySchaufelberger <67718063+TonySchaufelberger@users.noreply.github.com>
2020-09-11 15:03:22 +1200

    Add "0" option, and questions
    Added the "0" index for answers for each question, assigned to 'N/A', to account for recording skipped questions. Also added some complex questions.

--- variables.py
+++ variables.py
@@ -1,213 +1,255 @@
 differentiation_questions = {"easy": [
   {
-    "question": "differentiation:What is the conjugate of 1 + 2i?",
+    "question": "easy1",
+    "type": "Differentiation",
     "answers": {
-      "a": '2i',
-      "b": '(-1 - 2i)',
-      "c": '(-1 + 2i)',
-      "d": '(1 - 2i)'
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'd'
   },
   {
-    "question": "differentiation:Simplify (i^3).",
-    "answers": {
-      "a": '(-i)',
-      "b": '(i)',
-      "c": '(-1)',
-      "d": '(1)'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "differentiation:easy3",
+    "question": "easy2",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:easy4",
+    "question": "easy3",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:easy5",
+    "question": "easy4",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:easy6",
+    "question": "easy5",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:easy7",
+    "question": "easy6",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:easy8",
+    "question": "easy7",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "easy8",
+    "type": "Differentiation",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }],
         "medium": [
   {
-    "question": "differentiation:test",
+    "question": "test",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium1",
+    "question": "medium1",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium2",
+    "question": "medium2",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium3",
+    "question": "medium3",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium4",
+    "question": "medium4",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium5",
+    "question": "medium5",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium6",
+    "question": "medium6",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:medium7",
+    "question": "medium7",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }],
         "hard": [
   {
-    "question": "differentiation:hard1",
+    "question": "hard1",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:hard2",
+    "question": "hard2",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:hard3",
+    "question": "hard3",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:hard4",
+    "question": "hard4",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "differentiation:hard5",
+    "question": "hard5",
+    "type": "Differentiation",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }]
@@ -215,214 +257,256 @@ differentiation_questions = {"easy": [
 
 integration_questions = {"easy": [
   {
-    "question": "integration:What is the conjugate of 1 + 2i?",
+    "question": "easy1",
+    "type": "Integration",
     "answers": {
-      "a": '2i',
-      "b": '(-1 - 2i)',
-      "c": '(-1 + 2i)',
-      "d": '(1 - 2i)'
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'd'
   },
   {
-    "question": "integration:Simplify (i^3).",
-    "answers": {
-      "a": '(-i)',
-      "b": '(i)',
-      "c": '(-1)',
-      "d": '(1)'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "integration:easy3",
+    "question": "easy2",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:easy4",
+    "question": "easy3",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:easy5",
+    "question": "easy4",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:easy6",
+    "question": "easy5",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:easy7",
+    "question": "easy6",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:easy8",
+    "question": "easy7",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
+    },
+    "correct_answer": 'a'
+  },
+  {
+    "question": "easy8",
+    "type": "Integration",
+    "answers": {
+      "a": 'a',
+      "b": 'b',
+      "c": 'c',
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }],
         "medium": [
   {
-    "question": "integration:test",
+    "question": "test",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium1",
+    "question": "medium1",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium2",
+    "question": "medium2",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium3",
+    "question": "medium3",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium4",
+    "question": "medium4",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium5",
+    "question": "medium5",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium6",
+    "question": "medium6",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:medium7",
+    "question": "medium7",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }],
         "hard": [
   {
-    "question": "integration:hard1",
+    "question": "hard1",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:hard2",
+    "question": "hard2",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:hard3",
+    "question": "hard3",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:hard4",
+    "question": "hard4",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "integration:hard5",
+    "question": "hard5",
+    "type": "Integration",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }]
@@ -431,214 +515,256 @@ integration_questions = {"easy": [
 complex_questions = {
         "easy": [
   {
-    "question": "complex:What is the conjugate of 1 + 2i?",
+    "question": "What is the conjugate of 1 + 2i?",
+    "type": "Complex Numbers",
     "answers": {
       "a": '2i',
       "b": '(-1 - 2i)',
       "c": '(-1 + 2i)',
-      "d": '(1 - 2i)'
+      "d": '(1 - 2i)',
+      "0": 'N/A'
     },
     "correct_answer": 'd'
   },
   {
-    "question": "complex:Simplify (i^3).",
+    "question": "Simplify (i^3).",
+    "type": "Complex Numbers",
     "answers": {
       "a": '(-i)',
       "b": '(i)',
       "c": '(-1)',
-      "d": '(1)'
+      "d": '(1)',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:easy3",
+    "question": "Write (3-i)(i+2) in the form a+ib.",
+    "type": "Complex Numbers",
+    "answers": {
+      'a': 'i+2',
+      'b': '7+i',
+      'c': '-i-5',
+      'd': '4',
+      "0": 'N/A'
+    },
+    "correct_answer": 'b'
+  },
+  {
+    "question": "Express -1-i in polar form (π is pi).",
+    "type": "Complex Numbers",
+    "answers": {
+      "a": '√(2)cis(-3π/2)',
+      "b": '√(2)cis(-3π/4)',
+      "c": '√(2)cis(3π/2)',
+      "d": '√(2)cis(-3π/8)',
+      "0": 'N/A'
+    },
+    "correct_answer": 'b'
+  },
+  {
+    "question": "Express √(3)-i in polar form (π is pi).",
+    "type": "Complex Numbers",
+    "answers": {
+      "a": 'cis(-π/3})',
+      "b": 'cis(-π/over 6)',
+      "c": '2cis(-π/6)',
+      "d": '2cis(-π/3)',
+      "0": 'N/A'
+    },
+    "correct_answer": 'c'
+  },
+  {
+    "question": "easy6",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:easy4",
+    "question": "easy7",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:easy5",
+    "question": "easy8",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "complex:easy6",
-    "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "complex:easy7",
-    "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "complex:easy8",
-    "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }],
         "medium": [
   {
-    "question": "complex:test",
+    "question": "Express (1/2i)-(i/1-2i) in the form of a+bi.",
+    "type": "Complex Numbers",
     "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
+      "a": '(1/5)-(5/10)i',
+      "b": '(2/5)-(7/10)i',
+      "c": '(1/5)+(7/10)i',
+      "d": '(2/5)i-(5/10)',
+      "0": 'N/A'
+    },
+    "correct_answer": 'b'
+  },
+  {
+    "question": "Find the cubic equation if the three roots are z=-2,2+i,2-i.",
+    "type": "Complex Numbers",
+    "answers": {
+      "a": 'z^3-2z^2-z+4',
+      "b": 'z^3-z^2-5z+5',
+      "c": 'z^3-5z^2-z+6',
+      "d": 'z^3-2z^2-3z+10',
+      "0": 'N/A'
+    },
+    "correct_answer": 'd'
+  },
+  {
+    "question": "If u=-4+3i and w=2+2i, find w/u.",
+    "type": "Complex Numbers",
+    "answers": {
+      "a": '(-2-i)/5',
+      "b": '(-1-12i)/25',
+      "c": '(-2-14i)/25',
+      "d": '(-1-14i)/5',
+      "0": 'N/A'
+    },
+    "correct_answer": 'c'
+  },
+  {
+    "question": "If √(5+12i)=a+ib, find a and b.",
+    "type": "Complex Numbers",
+    "answers": {
+      "a": 'a=3,b=2',
+      "b": 'a=-3,b=2',
+      "c": 'a=3,b=-2',
+      "d": 'a=-3,b=1',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:medium1",
+    "question": "medium4",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:medium2",
+    "question": "medium5",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:medium3",
+    "question": "medium6",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:medium4",
+    "question": "medium7",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "complex:medium5",
-    "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "complex:medium6",
-    "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
-    },
-    "correct_answer": 'a'
-  },
-  {
-    "question": "complex:medium7",
-    "answers": {
-      "a": 'a',
-      "b": 'b',
-      "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }],
         "hard": [
   {
-    "question": "complex:hard1",
+    "question": "hard1",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:hard2",
+    "question": "hard2",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:hard3",
+    "question": "hard3",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:hard4",
+    "question": "hard4",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   },
   {
-    "question": "complex:hard5",
+    "question": "hard5",
+    "type": "Complex Numbers",
     "answers": {
       "a": 'a',
       "b": 'b',
       "c": 'c',
-      "d": 'd'
+      "d": 'd',
+      "0": 'N/A'
     },
     "correct_answer": 'a'
   }]
